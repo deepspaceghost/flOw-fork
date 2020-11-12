d3261fa2e36a547dda747cfdd0eacf3a5275b5d8
Hemingway <ghostblac@protonmail.com>
2020-11-12 03:51:12 -0500

    Delete behavior.py

--- behavior.py
+++
@@ -1,7 +0,0 @@
-class Behavior
-   def behavior():
-		    panicColor = new XColor(0x00BFFF)
-		    targetPlayerColor = new XColor(0xFF8844)
-		    targetFoodColor = new XColor(0xFFFFFF)
-		    targetFishColor = new XColor(0xFF8844)
-		    defaultColor = new XColor(0xFFFFFF)
